a85320e2628786b26be6d3dcc8c113f518ea0325
mmaguir2 <93003041+mmaguir2@users.noreply.github.com>
2022-02-13 18:03:15 -0500

    Update script.js

--- data/script.js
+++ data/script.js
@@ -31,7 +31,7 @@ function onClose(event) {
 }
 
 function updateSliderPWM(element) {
-    var sliderNumber = element.id.charAt(element.id.length-1);
+    var sliderNumber = element.id.charAt(element.id.length-2)+element.id.charAt(element.id.length-1);
     var sliderValue = document.getElementById(element.id).value;
     document.getElementById("sliderValue"+sliderNumber).innerHTML = sliderValue;
     console.log(sliderValue);
@@ -42,13 +42,13 @@ function onMessage(event) {
     console.log(event.data);
     var myObj = JSON.parse(event.data);
     var keys = Object.keys(myObj);
-
     for (var i = 0; i < keys.length; i++){
         var key = keys[i];
         document.getElementById(key).innerHTML = myObj[key];
         document.getElementById("slider"+ (i+1).toString()).value = myObj[key];
     }
 }
+
 function logoutButton() {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "/logout", true);
@@ -57,7 +57,7 @@ function logoutButton() {
 }
 
 function resetButton(){
-	for(var i=1; i<10;i++){
+	for(var i=10; i<27;i++){
 	//changes slider Value: to zero
 	document.getElementById("sliderValue"+i.toString()).innerHTML = "0";
 	//changes slider to postion zero
@@ -67,45 +67,60 @@ function resetButton(){
 }
 
 function bOne(){
-	var i = 1;
-	//changes slider Value: to zero
+	var i = 10;
+	//changes slider Value: to 5
 	document.getElementById("sliderValue"+i.toString()).innerHTML = "5";
-	//changes slider to postion zero
+	//changes slider to postion 5
 	document.getElementById("slider"+i.toString()).value = "5";
-	i = 2;
+	i = 11;
 	document.getElementById("sliderValue"+i.toString()).innerHTML = "10";
-	//changes slider to postion zero
 	document.getElementById("slider"+i.toString()).value = "10";
-	i = 3;
+	i = 12;
 	document.getElementById("sliderValue"+i.toString()).innerHTML = "15";
-	//changes slider to postion zero
 	document.getElementById("slider"+i.toString()).value = "15";
-	i = 4;
+	i = 13;
 	document.getElementById("sliderValue"+i.toString()).innerHTML = "20";
-	//changes slider to postion zero
 	document.getElementById("slider"+i.toString()).value = "20";
-	i = 5;
+	i = 14;
 	document.getElementById("sliderValue"+i.toString()).innerHTML = "25";
-	//changes slider to postion zero
 	document.getElementById("slider"+i.toString()).value = "25";
-	i = 6;
+	i = 15;
 	document.getElementById("sliderValue"+i.toString()).innerHTML = "30";
-	//changes slider to postion zero
 	document.getElementById("slider"+i.toString()).value = "30";
-	i = 7;
+	i = 16;
 	document.getElementById("sliderValue"+i.toString()).innerHTML = "35";
-	//changes slider to postion zero
 	document.getElementById("slider"+i.toString()).value = "35";
-	i = 8;
+	i = 17;
 	document.getElementById("sliderValue"+i.toString()).innerHTML = "40";
-	//changes slider to postion zero
 	document.getElementById("slider"+i.toString()).value = "40";
-	i = 9;
+	i = 18;
 	document.getElementById("sliderValue"+i.toString()).innerHTML = "45";
-	//changes slider to postion zero
 	document.getElementById("slider"+i.toString()).value = "45";
-	
-	for(var i=1; i<10;i++){
+	i = 19;
+	document.getElementById("sliderValue"+i.toString()).innerHTML = "50";
+	document.getElementById("slider"+i.toString()).value = "50";
+	i = 20;
+	document.getElementById("sliderValue"+i.toString()).innerHTML = "55";
+	document.getElementById("slider"+i.toString()).value = "55";
+	i = 21;
+	document.getElementById("sliderValue"+i.toString()).innerHTML = "60";
+	document.getElementById("slider"+i.toString()).value = "60";
+	i = 22;
+	document.getElementById("sliderValue"+i.toString()).innerHTML = "65";
+	document.getElementById("slider"+i.toString()).value = "65";
+	i = 23;
+	document.getElementById("sliderValue"+i.toString()).innerHTML = "70";
+	document.getElementById("slider"+i.toString()).value = "70";
+	i = 24;
+	document.getElementById("sliderValue"+i.toString()).innerHTML = "75";
+	document.getElementById("slider"+i.toString()).value = "75";
+	i = 25;
+	document.getElementById("sliderValue"+i.toString()).innerHTML = "80";
+	document.getElementById("slider"+i.toString()).value = "80";
+	i = 26;
+	document.getElementById("sliderValue"+i.toString()).innerHTML = "85";
+	document.getElementById("slider"+i.toString()).value = "85";
+	for(var i=10; i<27;i++){
 	updateSliderPWM(document.getElementById("slider"+i.toString()));
 	}
 }
