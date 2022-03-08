3a497d18ec5d9f829a1799cbdb7021bbd6668155
mmaguir2 <93003041+mmaguir2@users.noreply.github.com>
2022-03-08 16:48:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -59,72 +59,36 @@ function logoutButton() {
 }
 
 function resetButton(){
-	for(var i=10; i<34;i++){
+	for(var i=10; i<27;i++){
 	//changes slider Value: to zero
 	document.getElementById("sliderValue"+i.toString()).innerHTML = "0";
 	//changes slider to postion zero
 	document.getElementById("slider"+i.toString()).value = "0";
 	}	
-	for(var i=10; i<27;i++){
+	for(var i=10; i<23;i++){
 	updateSliderPWM(document.getElementById("slider"+i.toString()));
 	}
 }
 
 function bOne(){
-	var i = 10;
 	//changes slider Value: to 5
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "5";
+	document.getElementById("sliderValue10").innerHTML = "5";
 	//changes slider to postion 5
-	document.getElementById("slider"+i.toString()).value = "5";
-	i = 11;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "10";
-	document.getElementById("slider"+i.toString()).value = "10";
-	i = 12;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "15";
-	document.getElementById("slider"+i.toString()).value = "15";
-	i = 13;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "20";
-	document.getElementById("slider"+i.toString()).value = "20";
-	i = 14;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "25";
-	document.getElementById("slider"+i.toString()).value = "25";
-	i = 15;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "30";
-	document.getElementById("slider"+i.toString()).value = "30";
-	i = 16;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "35";
-	document.getElementById("slider"+i.toString()).value = "35";
-	i = 17;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "40";
-	document.getElementById("slider"+i.toString()).value = "40";
-	i = 18;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "45";
-	document.getElementById("slider"+i.toString()).value = "45";
-	i = 19;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "50";
-	document.getElementById("slider"+i.toString()).value = "50";
-	i = 20;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "55";
-	document.getElementById("slider"+i.toString()).value = "55";
-	i = 21;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "60";
-	document.getElementById("slider"+i.toString()).value = "60";
-	i = 22;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "65";
-	document.getElementById("slider"+i.toString()).value = "65";
-	i = 23;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "70";
-	document.getElementById("slider"+i.toString()).value = "70";
-	i = 24;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "75";
-	document.getElementById("slider"+i.toString()).value = "75";
-	i = 25;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "80";
-	document.getElementById("slider"+i.toString()).value = "80";
-	i = 26;
-	document.getElementById("sliderValue"+i.toString()).innerHTML = "85";
-	document.getElementById("slider"+i.toString()).value = "85";
-	for(var i=10; i<27;i++){
+	document.getElementById("slider10").value = "5";
+	
+	document.getElementById("sliderValue11").innerHTML = "10";
+	document.getElementById("slider11").value = "10";
+	
+	document.getElementById("sliderValue12").innerHTML = "15";
+	document.getElementById("slider12").value = "15";
+
+	document.getElementById("sliderValue13").innerHTML = "20";
+	document.getElementById("slider13").value = "20";
+
+	document.getElementById("sliderValue14").innerHTML = "25";
+	document.getElementById("slider14").value = "25";
+	//only update servos of slider values that dont move two at a time
+	for(var i=10; i<15;i++){
 	updateSliderPWM(document.getElementById("slider"+i.toString()));
 	}
 }
@@ -176,22 +140,8 @@ function palatoglossusLR(element) {
 		updateSliderPWM(document.getElementById("slider"+i.toString()));
 	}
 }
-function transversusLR(element) {
-    var sliderNumber = element.id.charAt(element.id.length-2)+element.id.charAt(element.id.length-1);
-    var sliderValue = document.getElementById(element.id).value;
-	//change slider Value: 
-    document.getElementById("sliderValue"+sliderNumber).innerHTML = sliderValue;
-	//change left and right slider Value:
-	document.getElementById("sliderValue19").innerHTML = sliderValue;
-	document.getElementById("sliderValue20").innerHTML = sliderValue;
-	//move  left and right slider position
-	document.getElementById("slider19").value = sliderValue;
-	document.getElementById("slider20").value = sliderValue;
-    for(var i = 19; i<21; i++){
-		updateSliderPWM(document.getElementById("slider"+i.toString()));
-	}
-}
-function verticalisLR(element) {
+
+function superiorInferior(element) {
     var sliderNumber = element.id.charAt(element.id.length-2)+element.id.charAt(element.id.length-1);
     var sliderValue = document.getElementById(element.id).value;
 	//change slider Value: 
@@ -203,37 +153,6 @@ function verticalisLR(element) {
 	document.getElementById("slider21").value = sliderValue;
 	document.getElementById("slider22").value = sliderValue;
     for(var i = 21; i<23; i++){
-		updateSliderPWM(document.getElementById("slider"+i.toString()));
-	}
-}
-
-function superiorLR(element) {
-    var sliderNumber = element.id.charAt(element.id.length-2)+element.id.charAt(element.id.length-1);
-    var sliderValue = document.getElementById(element.id).value;
-	//change slider Value: 
-    document.getElementById("sliderValue"+sliderNumber).innerHTML = sliderValue;
-	//change left and right slider Value:
-	document.getElementById("sliderValue23").innerHTML = sliderValue;
-	document.getElementById("sliderValue24").innerHTML = sliderValue;
-	//move  left and right slider position
-	document.getElementById("slider23").value = sliderValue;
-	document.getElementById("slider24").value = sliderValue;
-    for(var i = 23; i<25; i++){
-		updateSliderPWM(document.getElementById("slider"+i.toString()));
-	}
-}
-function inferiorLR(element) {
-    var sliderNumber = element.id.charAt(element.id.length-2)+element.id.charAt(element.id.length-1);
-    var sliderValue = document.getElementById(element.id).value;
-	//change slider Value: 
-    document.getElementById("sliderValue"+sliderNumber).innerHTML = sliderValue;
-	//change left and right slider Value:
-	document.getElementById("sliderValue25").innerHTML = sliderValue;
-	document.getElementById("sliderValue26").innerHTML = sliderValue;
-	//move  left and right slider position
-	document.getElementById("slider25").value = sliderValue;
-	document.getElementById("slider26").value = sliderValue;
-    for(var i = 25; i<26; i++){
 		updateSliderPWM(document.getElementById("slider"+i.toString()));
 	}
 }
